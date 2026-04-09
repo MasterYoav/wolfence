5c1e1b6741c7c19c1356e338320d97ac9288ee35
MasterYoav <realyoavperetz@gmail.com>
2026-04-10 00:22:38 +0300

    Initial Commit

---
+++ Wolfence/WolfenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  WolfenceApp.swift
+//  Wolfence
+//
+//  Created by Yoav Peretz on 10/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct WolfenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WolfenceTests/WolfenceTests.swift
@@ -0,0 +1,19 @@
+//
+//  WolfenceTests.swift
+//  WolfenceTests
+//
+//  Created by Yoav Peretz on 10/04/2026.
+//
+
+import Testing
+@testable import Wolfence
+
+struct WolfenceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
